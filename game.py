404ea42e80ef45e1c3f4f4b1e8702a77b3e6f540
usjamesmc <usjamesmc@gmail.com>
2025-11-20 11:08:11 -0600

    Adjusted round phase function for 1 player option, RPSLS complete.

--- game.py
+++ game.py
@@ -22,12 +22,11 @@ class Game:
         print('Spock beats Rock and Scissors')
 
     def choose_opponent(self):
-        opponent_choice = (input('Will there be 1 or 2 players?'))
-        if opponent_choice == '1':
+        self.opponent_choice = (input('Will there be 1 or 2 players?'))
+        if self.opponent_choice == '1':
             self.player_two = Computer('Tim')
-            Computer.randomize_gesture(self.player_two)
-        elif opponent_choice == '2':
-            opponent_choice = '2'
+        elif self.opponent_choice == '2':
+            self.opponent_choice = '2'
         else:
             print('Incorrect entry, try again')
             self.choose_opponent()
@@ -35,30 +34,54 @@ class Game:
     def round_phase(self):
 
         while self.player_one.score < 2 and self.player_two.score < 2:
-            self.player_one.choose_gesture()
-            self.player_two.choose_gesture()
-            #rework for when computer is used, dont need a second choose gesture here for that
-            if self.player_one.chosen_gesture == 'Rock' and self.player_two.chosen_gesture in ['Scissors', 'Lizard']:
-                print('Player one wins this round')
-                self.player_one.score += 1
-            elif self.player_one.chosen_gesture == 'Paper' and self.player_two.chosen_gesture in ['Rock', 'Spock']:
-                print('Player one wins this round')
-                self.player_one.score += 1
-            elif self.player_one.chosen_gesture == 'Scissors' and self.player_two.chosen_gesture in ['Paper', 'Lizard']:
-                print('Player one wins this round')
-                self.player_one.score += 1
-            elif self.player_one.chosen_gesture == 'Lizard' and self.player_two.chosen_gesture in ['Paper', 'Spock']:
-                print('Player one wins this round')
-                self.player_one.score += 1
-            elif self.player_one.chosen_gesture == 'Spock' and self.player_two.chosen_gesture in ['Rock', 'Scissors']:
-                print('Player one wins this round')
-                self.player_one.score += 1
-            elif self.player_one.chosen_gesture == self.player_two.chosen_gesture:
-                self.player_one.chosen_gesture = self.player_two.chosen_gesture
-                print('Both players chose the same gesture, keep playing!')
-            else:
-                self.player_two.score += 1
-                print('Player two wins this round')
+            if self.opponent_choice == '2':
+                self.player_one.choose_gesture()
+                self.player_two.choose_gesture()
+                if self.player_one.chosen_gesture == 'Rock' and self.player_two.chosen_gesture in ['Scissors', 'Lizard']:
+                    print('Player one wins this round')
+                    self.player_one.score += 1
+                elif self.player_one.chosen_gesture == 'Paper' and self.player_two.chosen_gesture in ['Rock', 'Spock']:
+                    print('Player one wins this round')
+                    self.player_one.score += 1
+                elif self.player_one.chosen_gesture == 'Scissors' and self.player_two.chosen_gesture in ['Paper', 'Lizard']:
+                    print('Player one wins this round')
+                    self.player_one.score += 1
+                elif self.player_one.chosen_gesture == 'Lizard' and self.player_two.chosen_gesture in ['Paper', 'Spock']:
+                    print('Player one wins this round')
+                    self.player_one.score += 1
+                elif self.player_one.chosen_gesture == 'Spock' and self.player_two.chosen_gesture in ['Rock', 'Scissors']:
+                    print('Player one wins this round')
+                    self.player_one.score += 1
+                elif self.player_one.chosen_gesture == self.player_two.chosen_gesture:
+                    self.player_one.chosen_gesture = self.player_two.chosen_gesture
+                    print('Both players chose the same gesture, keep playing!')
+                else:
+                    self.player_two.score += 1
+                    print('Player two wins this round')
+            elif self.opponent_choice == '1':
+                self.player_one.choose_gesture()
+                self.player_two.randomize_gesture()
+                if self.player_one.chosen_gesture == 'Rock' and self.player_two.chosen_gesture in ['Scissors', 'Lizard']:
+                    print('Player one wins this round')
+                    self.player_one.score += 1
+                elif self.player_one.chosen_gesture == 'Paper' and self.player_two.chosen_gesture in ['Rock', 'Spock']:
+                    print('Player one wins this round')
+                    self.player_one.score += 1
+                elif self.player_one.chosen_gesture == 'Scissors' and self.player_two.chosen_gesture in ['Paper', 'Lizard']:
+                    print('Player one wins this round')
+                    self.player_one.score += 1
+                elif self.player_one.chosen_gesture == 'Lizard' and self.player_two.chosen_gesture in ['Paper', 'Spock']:
+                    print('Player one wins this round')
+                    self.player_one.score += 1
+                elif self.player_one.chosen_gesture == 'Spock' and self.player_two.chosen_gesture in ['Rock', 'Scissors']:
+                    print('Player one wins this round')
+                    self.player_one.score += 1
+                elif self.player_one.chosen_gesture == self.player_two.chosen_gesture:
+                    self.player_one.chosen_gesture = self.player_two.chosen_gesture
+                    print('Both players chose the same gesture, keep playing!')
+                else:
+                    self.player_two.score += 1
+                    print('Player two wins this round')
     
             
 
